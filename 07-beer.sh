532970f310bc31685c6ec61e1a3c2efc7750b78d
Dion Del Rosario <dndelro@gmail.com>
2023-09-03 22:24:02 -0400

    Update 07-beer.sh
    Updated beer to have days of christmas song

--- 07-beer.sh
+++ 07-beer.sh
@@ -1,24 +1,49 @@
 #!/bin/sh
 
-echo "Let's sing a beer song"
-echo "How many bottles?"
-read count
+echo "Let's sing the 12 Days of Christmas!"
 
-while [ $count -ge 0 ]; do
-  if [ $count -ge 2 ]; then
-      echo "$count bottles of beer on the wall, $count bottles of beer"
-      echo "Take one down pass it around"
-  elif [ $count -eq 1 ]; then
-      echo "$count bottle of beer on the wall, $count bottles of beer"
-      echo "Take one down pass it around"
-  else
-      echo "no more bottles of beer on the wall"
-  fi
-  
-  # the following statement is equivalent to: let "count=count-1"
-  ((count = count - 1))
+for day in $(seq 1 12); do
+    echo "On the $day day of Christmas, my true love gave to me:"
+
+    case $day in
+        12) echo "12 drummers drumming,";;
+        11) echo "11 pipers piping,";;
+        10) echo "10 lords a-leaping,";;
+        9) echo "9 ladies dancing,";;
+        8) echo "8 maids a-milking,";;
+        7) echo "7 swans a-swimming,";;
+        6) echo "6 geese a-laying,";;
+        5) echo "5 golden rings,";;
+        4) echo "4 calling birds,";;
+        3) echo "3 French hens,";;
+        2) echo "2 turtle doves, and";;
+        1) 
+            if [ $day -eq 1 ]; then
+                echo "A partridge in a pear tree."
+            else
+                echo "A partridge in a pear tree,"
+            fi
+            ;;
+    esac
+
+    # loop back to print the gifts from previous days
+    for prev_day in $(seq $((day-1)) -1 1); do
+        case $prev_day in
+            12) echo "12 drummers drumming,";;
+            11) echo "11 pipers piping,";;
+            10) echo "10 lords a-leaping,";;
+            9) echo "9 ladies dancing,";;
+            8) echo "8 maids a-milking,";;
+            7) echo "7 swans a-swimming,";;
+            6) echo "6 geese a-laying,";;
+            5) echo "5 golden rings,";;
+            4) echo "4 calling birds,";;
+            3) echo "3 French hens,";;
+            2) echo "2 turtle doves, and";;
+            1) echo "A partridge in a pear tree.";;
+        esac
+    done
+
+    echo ""  # add a newline for separation
 
 done
-
-# exercise: implement another counting song (such as 12 days of Christmas) 
-# using loops and if statements.
